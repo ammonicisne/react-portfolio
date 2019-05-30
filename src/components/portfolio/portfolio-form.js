3d5fb5426bc9c3e4bc4354d09065ff78862b79b3
Ammon Cisneros <ammon.15.2015.ci@gmail.com>
2019-05-30 10:36:59 -0700

    Created initial structure for components 30May2019

---
+++ src/components/portfolio/portfolio-form.js
@@ -0,0 +1,12 @@
+import React, { Componenet } from "react";
+
+export default class PortfolioForm extends Componenet {
+  render() {
+    return (
+      <div>
+        <h1>PortfolioForm</h1>
+        <h1>PortfolioForm</h1>
+      </div>
+    );
+  }
+}
