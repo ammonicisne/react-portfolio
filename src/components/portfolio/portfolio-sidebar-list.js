0918e94fe23a9036c5fac8df5232272811268046
Ammon Cisneros <ammon.15.2015.ci@gmail.com>
2019-05-30 10:22:43 -0700

    Updated styling for portfolio manager 30May2019

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -3,17 +3,17 @@ import React from "react";
 const PortfolioSidebarList = props => {
   const portfolioList = props.data.map(portfolioItem => {
     return (
-      <div>
-        <img src={portfolioItem.thumb_image_url} />
-        <div>
-          <h1>{portfolioItem.name}</h1>
-          <h1>{portfolioItem.id}</h1>
+      <div className="portfolio-item-thumb">
+        <div className="portfolio-thumb-img">
+          <img src={portfolioItem.thumb_image_url} />
         </div>
+        <h1>{portfolioItem.name}</h1>
+        <h1>{portfolioItem.id}</h1>
       </div>
     );
   });
 
-  return <div>{portfolioList}</div>;
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
 };
 
 export default PortfolioSidebarList;
