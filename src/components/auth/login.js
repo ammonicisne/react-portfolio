21771e4148120c982cb51401a42d5b609a670be3
Ammon Cisneros <ammon.15.2015.ci@gmail.com>
2019-05-30 09:56:56 -0700

    Updated app,auth,nav,pages,port,manager 30May2019

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Login extends Component {
   constructor(props) {
@@ -6,45 +7,79 @@ export default class Login extends Component {
 
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      errorText: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
+      errorText: ""
     });
   }
 
   handleSubmit(event) {
-    console.log("Handle Submit", event);
+    axios
+      .post(
+        "https://api.devcamp.space/sessions",
+        {
+          client: {
+            email: this.state.email,
+            password: this.state.password
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        if (response.data.status === "created") {
+          this.props.handleSuccessfulAuth();
+        } else {
+          this.setState({
+            errorText: "Wrong email or password"
+          });
+          this.props.handleUnsuccessfulAuth();
+        }
+      })
+      .catch(error => {
+        this.setState({
+          errorText: "An error occurred"
+        });
+        this.props.handleUnsuccessfulAuth();
+      });
+
+    event.preventDefault();
   }
+
   render() {
     return (
       <div>
-        <h1>dashboard login</h1>
+        <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+        <div>{this.state.errorText}</div>
 
         <form onSubmit={this.handleSubmit}>
           <input
             type="email"
             name="email"
-            placeholder="Email"
+            placeholder="Your email"
             value={this.state.email}
             onChange={this.handleChange}
           />
+
           <input
             type="password"
             name="password"
-            placeholder="Password"
+            placeholder="Your password"
             value={this.state.password}
             onChange={this.handleChange}
           />
 
           <div>
-            <button type="submit">login</button>
+            <button type="submit">Login</button>
           </div>
         </form>
       </div>
